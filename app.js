cf2b371c0b8a06f91e95fc14da646e0c0b8c9fd8
Rohit-Rana-01 <rohitrana3122000@gmail.com>
2025-09-16 13:39:44 +0530

    merge conflict practice

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-// hello world this change has done in feature 1  branch .....
+// hello world this change has done in feature 1  branch .....git
+// this is done for merge conflict practice --- IGNORE ---
